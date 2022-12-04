6f96ab69ca8db0c793bf64ac174c69158a14455f
NataliHTML <nataly07081992@gmail.com>
2022-12-04 19:18:09 +0300

    add desktop version

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,23 @@
+const swiper = new Swiper(".swiper", {
+  loop: false,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-right",
+    prevEl: ".swiper-button-left",
+  },
+});
+
 $(function () {
-
-
+  $(".questions__item-title").on("click", function () {
+    $(".questions__item").removeClass("questions__item--active");
+    $(this).parent().addClass("questions__item--active");
+  });
+  $("#fullpage").fullpage({
+    autoScrolling: true,
+    scrollHorizontally: true,
+    sectionSelector: ".page-section",
+  });
 });
