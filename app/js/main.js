bc7c92e0d8a2b265da4e2bca4126e09b55a51645
NataliHTML <nataly07081992@gmail.com>
2022-12-18 20:50:31 +0300

    add finish

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,5 @@
 const swiper = new Swiper(".swiper", {
   loop: false,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "fraction",
-  },
   navigation: {
     nextEl: ".swiper-button-right",
     prevEl: ".swiper-button-left",
@@ -19,5 +15,24 @@ $(function () {
     autoScrolling: true,
     scrollHorizontally: true,
     sectionSelector: ".page-section",
+    scrollOverflow: true,
+    menu: "#header__nav",
+    anchors: [
+      "top",
+      "products",
+      "benefits",
+      "specification",
+      "questions",
+      "contacts",
+    ],
+  });
+  $(".menu__btn").on("click", function () {
+    $(".menu__btn").toggleClass("menu__btn--active");
+    $(".menu__list").toggleClass("menu__list--active");
+  });
+
+  $(".menu__list-link").on("click", function () {
+    $(".menu__btn").removeClass("menu__btn--active");
+    $(".menu__list").removeClass("menu__list--active");
   });
 });
